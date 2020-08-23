64f26f14b485942c15e0bda875a763a99dac1c8a
Kenny <dang.kenny84@gmail.com>
2020-08-23 17:20:54 -0400

    Adding smart outlet

--- SmartOutlet/static/js/myjava.js
+++ SmartOutlet/static/js/myjava.js
@@ -1,11 +1,27 @@
 function toggle_outlet(){
-	$.ajax({
+	var outlet = document.getElementById("office");
+	var class_list = outlet.classList.toString();	
+	if (class_list.includes("on")){
+		outlet.classList.remove("on");
+		$.ajax({
 			type: 'GET',
 			url: "toggle_outlet/",
-			data: {"data": 1},
+			data: {"command": "off"},
 			success: function(response){				
 			},
 			error: function(response){
 			}
 		})
+	}else{	
+		outlet.classList.add("on");
+		$.ajax({
+			type: 'GET',
+			url: "toggle_outlet/",
+			data: {"command": "on"},
+			success: function(response){				
+			},
+			error: function(response){
+			}
+		})
+	}
 }
--- SmartOutlet/views.py
+++ SmartOutlet/views.py
@@ -6,7 +6,8 @@ import time
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello from the home page!")
+	my_dict = {'key': 'value'}
+	return render(request, 'SmartOutlet/home.html', context=my_dict)
 
 def outlets(request):
 	my_dict = {'key': 'value'}
@@ -14,11 +15,13 @@ def outlets(request):
 
 def toggle_outlet(request):
 	if request.is_ajax:
+		command = request.GET.get('command', None)
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(18,GPIO.OUT)
-		GPIO.output(18, True)
-		time.sleep(1)
-		GPIO.output(18, False)
-		time.sleep(1)
-		GPIO.cleanup()
+		if command == "on":			
+			GPIO.output(18, True)
+		else:
+			GPIO.output(18, False)
+			GPIO.cleanup()
+	
 	return JsonResponse({"error": "", "status": 200})
